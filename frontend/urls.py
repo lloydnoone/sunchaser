b42f3181151a1923e6ab770d048f57c45588aca2
lloydnoone <lloydnoone92@gmail.com>
2019-11-27 20:55:30 +0000

    recommit

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, re_path
 from .views import Home, Assets
 
+# urlpatterns = [
+#     path('', Home.as_view()),
+#     re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view())
+# ]
+
 urlpatterns = [
-    path('', Home.as_view()),
-    re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view())
+    re_path(r'^[\w\?\=\-\/]*$', Home.as_view()),
+    re_path(r'(?P<filename>(assets/)?[\w\.\-\/]+)$', Assets.as_view()),
 ]
